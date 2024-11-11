660ed26b7e1ba9a8fbfbf58fce81a4455568b235
Dominic Daniel Krämer <Dominicdaniel3107@gmail.com>
2024-11-11 22:36:30 +0100

    Add method to get issue list from GitHub

--- src/read_in_github_format.py
+++ src/read_in_github_format.py
@@ -34,3 +34,39 @@ def read_from_gitlab(url, token):
     except requests.exceptions.RequestException as e:
         print("An error occured", e)
 
+
+def read_from_github(url, token):
+    headers = { 'Authorization': f'Bearer {token}'}
+    issues = []
+    try:
+        response = requests.get(url,
+                                headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+
+            for d in data:
+                id = d['number']
+                title = d['title']
+                # description: TODO
+                if d['milestone'] is not None:
+                    milestone = d['milestone']['number']
+                else:
+                    milestone = None
+                labels = []
+                for l in d['labels']:
+                    labels.append(l['name'])
+                assignees = []
+                for a in d['assignees']:
+                    assignees.append(a['login'])
+                state = d['state']
+                state_reason = d['state_reason'] # might be None
+
+                new_issue = GitHubIssue(id, title, "description", milestone, labels, assignees, state, state_reason)
+                issues.append(new_issue)
+            return issues
+
+        else:
+            print("failed to retrieve data (status code: " + str(response.status_code) + ")")
+
+    except requests.exceptions.RequestException as e:
+        print("An error occured", e)
