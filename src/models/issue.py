352873cdbf4c4e6ac8b106b8b19847eecd153ca5
Dominic Daniel Krämer <Dominicdaniel3107@gmail.com>
2024-11-15 01:38:12 +0100

    Refactor code: implement better structure for issue class and upcoming classes

---
+++ src/models/issue.py
@@ -0,0 +1,49 @@
+class Issue:
+    def __init__(self, id, title, description, labels, state, milestone, assignees):
+        self.id = id # int
+        self.title = title # str
+        self.description = description # str
+        self.labels = labels # list
+        self.state = state # str
+        self.milestone = milestone # str
+        self.assignees = assignees # list
+
+    def __str__(self):
+        return (f"Issue ID: {self.id}\n"
+                f"Title: {self.title}\n"
+                f"Description: {self.description}\n"
+                f"Labels: {', '.join(self.labels)}\n"
+                f"State:  {self.state}\n"
+                f"Milestone: {self.milestone}\n"
+                f"Assignees: {', '.join(self.assignees)}\n")
+
+    def __eq__(self, other):
+        if not isinstance(other, Issue):
+            return NotImplemented
+
+        return (self.id == other.id and
+                self.title == other.title and
+                # self.description == other.description and
+                self.labels == other.labels and
+                self.state == other.state and
+                self.milestone == other.milestone and
+                self.assignees == other.assignees)
+
+    def to_add_dict(self):
+        return {
+            'title': self.title,
+            # 'body': self.description,
+            'labels': self.labels,
+            'milestone': self.milestone,
+            'assignees': self.assignees
+        }
+
+    def to_modify_dict(self):
+        return {
+            'title': self.title,
+            # 'body': self.description,
+            'labels': self.labels,
+            'state': self.state,
+            'milestone': self.milestone,
+            'assignees': self.assignees
+        }
