83735cb436e50c507fc798a72022c91e318e1a7e
Dominic Daniel Krämer <Dominicdaniel3107@gmail.com>
2024-12-10 16:16:44 +0100

    Add issue lists to test repo deployment scripts

---
+++ src/tests/deploy_test_repo/create_github_test_repo.py
@@ -0,0 +1,9 @@
+issues_to_add = [
+    {"title": "Issue1"},
+    {"title": "Issue2"},
+    {"title": "issue3"},
+    {"title": "Issue4"},
+    {"title": "Issue5"},
+    {"title": "DELETE THIS"},
+    {"title": "DELETE THIS"}
+]
